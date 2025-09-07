5a98d86214d8c5b4a0b49aa60dcc39f62cdec723
GodofPottans <ivanchittissery@gmail.com>
2025-09-07 13:16:46 -0400

    Update Terra_cats_machine_learning_module.py

--- Terra_cats_machine_learning_module.py
+++ Terra_cats_machine_learning_module.py
@@ -14,26 +14,26 @@ hub = PrimeHub()
 #ColorSensor2 Port:F
 
 #Motor defining#
-left_motor = Motor(Port.A, Direction=ANTICLOCKWISE) #Defined left_motor var. as a motor and at port A with an anitclockwise direction
-right_motor = Motor(Port.B, Direction=CLOCKWISE) #Defined left_motor var. as a motor and at port A with an anitclockwise direction
-attach_motor1 = Port.C
-attach_motor2 = Port.D
+left_motor = Motor(Port.A, Direction.ANTICLOCKWISE) #Defined left_motor var. as a motor and at port A with an anitclockwise direction
+right_motor = Motor(Port.B, Direction.CLOCKWISE) #Defined left_motor var. as a motor and at port A with an anitclockwise direction
+attach_motor1 = Motor(Port.C)
+attach_motor2 = Motor(Port.D)
 ColorSensor1 = ColorSensor(Port.E)
 ColorSensor2 = ColorSensor(Port.F)
 
 Gyro_Drive = GyroDriveBase(left_motor, right_motor, wheel_diameter=70, axle_track=150) #requires the left and right motr, the wheel diameter, and the distance from one wheel to another(mostly from ground touvhing point to the other)
 navigation_data = DataLog('time', 'angle')
 Attachment1_data = DataLog('time', 'attachement1 angle')
-Attachment1_data = DataLog('time', 'attachement2 angle')
+Attachment2_data = DataLog('time', 'attachement2 angle')
 watch = StopWatch()
 for i in range(1000):
     Gyro_angle= hub.imu.heading()
     Attachment1_angle = attach_motor1.angle()
     Attachment2_angle = attach_motor2.angle()
     time = watch.time()
-    distance = distance()
+    distance = GyroBaseDrive.distance()
     navigation_data.log(time, distance, Gyro_angle) #Logging navigation data for moving around
     Attachment1_data.log(time, distance, Attachment1_angle) #logging the first motor for attachments
     Attachment2_data.log(time, distance, Attachment2_angle) #logging the first motor for attachments
-    multitask(print(time), print('|'), print(Gyro_angle), print('|'), print(Attachment1_angle), print('|'), print(Attachment2_angle))
+    print(time, "|", distance, "|", Gyro_angle, "|", Attachment1_angle, "|", Attachment2_angle)
     wait(150)
